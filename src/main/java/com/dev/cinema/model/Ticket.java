60f2d2783cac35f168b52449f107443f2b2dca1d
Nokafan <volodymy.stepura@gmail.com>
2020-10-12 11:49:49 +0300

    Changed annotation

--- src/main/java/com/dev/cinema/model/Ticket.java
+++ src/main/java/com/dev/cinema/model/Ticket.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +21,7 @@ public class Ticket {
     @Column(name = "ticket_id")
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "session_id")
     private MovieSession movieSession;
 
