326ca9b9c011874d9581f102c908c77a64f60df4
Nokafan <volodymy.stepura@gmail.com>
2020-10-13 14:17:04 +0300

    Fixed mapping

--- src/main/java/com/dev/cinema/model/Order.java
+++ src/main/java/com/dev/cinema/model/Order.java
@@ -8,8 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,8 +24,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToMany
-    @JoinColumn(name = "ticket_id")
+    @OneToMany
     private List<Ticket> tickets;
 
     @Column(name = "order_date")
